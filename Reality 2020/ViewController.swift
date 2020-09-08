f05b61eccb73cc2de6cefc4772e25628558e0f7b
Denis Bystruev <dbystruev@me.com>
2020-09-08 21:03:12 +0300

    Initial Commit

---
+++ Reality 2020/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reality 2020
+//
+//  Created by Denis Bystruev on 08.09.2020.
+//  Copyright © 2020 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
